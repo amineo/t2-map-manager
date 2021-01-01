430e390478555a037ea87fa293a7640b5c031765
Anthony Mineo <anthonymineo@gmail.com>
2021-01-01 15:11:17 -0500

    Setup base appshell

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
-
+const colors = require('tailwindcss/colors');
+//const forms = require('tailwindcss/forms');
+//const typography = require('tailwindcss/typography');
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
@@ -7,9 +9,14 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'light-blue': colors.lightBlue,
+        cyan: colors.cyan,
+        rose: colors.rose
       }
     }
   },
   variants: {},
-  plugins: [],
+  plugins: [ ]
 };
