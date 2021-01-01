8924c3c9792b6e9a7e1ce97e21221c828ab82ef0
Anthony Mineo <anthonymineo@gmail.com>
2021-01-01 18:18:03 -0500

    Add css purge paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-const colors = require('tailwindcss/colors');
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 //const forms = require('tailwindcss/forms');
 //const typography = require('tailwindcss/typography');
 module.exports = {
-  purge: [],
+  purge: ["./src/**/*.html", "./src/**/*.tsx", "./src/**/*.jsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        'light-blue': colors.lightBlue,
+        "light-blue": colors.lightBlue,
         cyan: colors.cyan,
-        rose: colors.rose
-      }
-    }
+        rose: colors.rose,
+      },
+    },
   },
   variants: {},
-  plugins: [ ]
+  plugins: [],
 };
