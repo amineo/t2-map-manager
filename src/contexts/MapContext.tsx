989beec608ccfd6fbe224ddcb8b3e1aaf1624219
Anthony Mineo <anthonymineo@gmail.com>
2021-01-06 08:52:18 -0500

    Simplify context

--- src/contexts/MapContext.tsx
+++ src/contexts/MapContext.tsx
@@ -6,21 +6,13 @@ const MapContext = createContext({});
 const { Provider } = MapContext;
 
 const MapProvider: React.FC = ({children}) => {
-
-  const [badArchiveList, setBadArchiveList]: any[] = useState([]);
-  const [localMissionList, setLocalMissionList]: any[] = useState([]);
-  const [vl2ArchiveList, setVl2ArchiveList]: any[] = useState([]);
-  const [reconcileDLMaps, setReconcileDLMaps]: any[] = useState([]);
-
+  const [maps, setMaps]: any[] = useState({});
 
 	const runMapCheck = useCallback(
 		async () => {
 			try {
-      const m = await MapCheck();
-        setBadArchiveList(m.badArchiveList);
-        setLocalMissionList(m.localMissionList);
-        setVl2ArchiveList(m.vl2ArchiveList);
-        setReconcileDLMaps(m.reconcileDLMaps);
+        const m = await MapCheck();
+        setMaps(m);
 			} catch (err) {
 				console.error(err);
 			}
@@ -28,19 +20,13 @@ const MapProvider: React.FC = ({children}) => {
 		[ MapCheck ]
 	);
 
-
-
   useEffect(() => {
     runMapCheck()
   }, [runMapCheck]);
 
 
   return (
-    <Provider value={{
-      badArchiveList,
-      localMissionList,
-      vl2ArchiveList,
-      reconcileDLMaps
+    <Provider value={{...maps
     }}>
       {children}
     </Provider>
