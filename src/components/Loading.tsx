22f48dc7ab12ec295742d7ff4e02bcef5e9f1022
Anthony Mineo <anthonymineo@gmail.com>
2021-01-16 12:07:57 -0500

    Better loading screen

---
+++ src/components/Loading.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Loading (){
+  return (
+    <div className="bg-gray-900">
+      <div id="loading">
+        <div>G</div>
+        <div>N</div>
+        <div>I</div>
+        <div>D</div>
+        <div>A</div>
+        <div>O</div>
+        <div>L</div>
+      </div>
+    </div>
+)};
--- src/pages/MapSync.tsx
+++ src/pages/MapSync.tsx
@@ -2,6 +2,7 @@ import React, { useContext}  from "react";
 
 import {IMapSync} from "../types";
 
+import Loading from '../components/Loading';
 import SyncStatus from "../components/SyncStatus";
 import SyncMapList from "../components/SyncMapList";
 import LocalMapList from "../components/LocalMapList";
@@ -23,11 +24,11 @@ export default function MapSyncPage (){
   return (
 
     <main
-      className="flex-1 relative z-0 overflow-y-auto focus:outline-none"
+      className={`flex-1 relative z-0 overflow-y-auto focus:outline-none ${mapContext.isLoading ? 'bg-gray-900': ''}`}
       tabIndex={-1}
     >
       { mapContext.isLoading ?
-        <div>Loading</div>
+        <Loading />
       : <>
         <button onClick={() => resync() }>Reload</button>
         <SyncStatus {...mapContext.mapDiffs} />
